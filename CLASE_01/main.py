65f2c0688f595d2b27844cb76210e471ea8b8f4c
Mauricio <m.davila@sistema-utnfra.com.ar>
2022-08-25 20:42:34 -0300

    Clase 01

---
+++ CLASE_01/main.py
@@ -0,0 +1,20 @@
+
+lista_nombres = []
+for i in range(5):
+    nombre = input("Nombre? : ")
+    lista_nombres.append(nombre)
+
+#ESTO SI
+for nombre in lista_nombres:
+    print(nombre)
+
+#ESTO NO
+for i in range(5):
+    print(lista_nombres[i])
+
+print(i)    
+
+variable = True
+if variable :
+    print("SIIIII")
+
