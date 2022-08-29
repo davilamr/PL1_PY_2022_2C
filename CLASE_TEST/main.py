7c339307b7576d0b490ee7c4bdd513d0c2525976
Mauricio <m.davila@sistema-utnfra.com.ar>
2022-08-29 10:07:36 -0300

    test

--- CLASE_TEST/main.py
+++ CLASE_TEST/main.py
@@ -1,1 +1,6 @@
-print("test")
+
+archivo = open('./CLASE_TEST/archivo.txt', 'w')
+archivo.write('Primer linea de texto\n')
+archivo.write('segunda linea\n')
+archivo.write('tercera linea\n')
+archivo.close()
